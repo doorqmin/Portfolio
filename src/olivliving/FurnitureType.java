599b58936a229d6b541b11afc9bece1d38a0060d
MoonGyumin <doorqmin@gmail.com>
2021-08-09 10:16:05 +0900

    Start! OliveLiving for Portfolio!

---
+++ src/olivliving/FurnitureType.java
@@ -0,0 +1,5 @@
+package olivliving;
+
+	public enum FurnitureType {
+		식탁, 서랍장, 식기보관함, 의자
+	}
---
+++ src/olivliving/OlivLiving.java
@@ -0,0 +1,11 @@
+package olivliving;
+
+public class OlivLiving {
+
+	public static void main(String[] args) {
+		FurnitureType tea = FurnitureType.식탁;
+		System.out.println("OlivLiving...당신의 삶에 편안함을 더합니다.");
+		System.out.println(FurnitureType.의자);
+	}
+
+}
